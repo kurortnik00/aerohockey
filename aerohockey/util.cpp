b41b969620242a90a79ea269190de2bad3608621
Nicholas Kapralov <4dvlup@gmail.com>
2019-12-03 16:19:48 +0300

    Fixed getcwd for Windows

--- aerohockey/util.cpp
+++ aerohockey/util.cpp
@@ -1,15 +1,34 @@
-#include <unistd.h>
 #include "util.hpp"
 
+#define RUNNING_WINDOWS
 
-int PATH_MAX = 255;
+#ifdef RUNNING_WINDOWS
+	#include <direct.h> // _getcwd
+	std::string getcwd_string() {
+		char* buffer;
 
-std::string getcwd_string() {
-   char buff[PATH_MAX];
-   getcwd(buff, PATH_MAX);
-   std::string cwd(buff);
-   return cwd;
-}
+		if ((buffer = _getcwd(NULL, 0)) == NULL)
+		{
+			std::cout << "Failed to retrieve CWD, problems with texture loading may occur." << std::endl;
+			return "";
+		}
+		else
+		{
+			std::cout << "Current working directory: " << std::string(buffer);
+			return std::string(buffer);
+			free(buffer);
+		}
+	}
+#elif defined RUNNING_LINUX
+	#include <unistd.h> // getcwd
+	std::string getcwd_string() {
+		int PATH_MAX = 255;
+		char buff[PATH_MAX];
+		getcwd(buff, PATH_MAX);
+		std::string cwd(buff);
+		return cwd;
+	}
+#endif
 
 
 float len2 (sf::Vector2f const v)
--- aerohockey/util.hpp
+++ aerohockey/util.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 
 std::string getcwd_string();
 float len2 (sf::Vector2f const v);
